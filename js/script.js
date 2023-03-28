0d70e805cbc9603741985bb29e33ef8301f5e580
ninajohnstone <118253390+ninajohnstone@users.noreply.github.com>
2023-03-28 11:37:22 +0200

    4 sidor, börjat optimera 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // Denna fil skall innehålla JavaScript (JS)
+
+const hamburger = document.querySelector("hamburger");
+const nav = document.querySelector("nav");
+
+function toggleMenu() {
+    nav.classList.toggle("active");
+}
